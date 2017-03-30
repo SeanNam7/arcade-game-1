0f9bd43bcdeb11b36712ed67d28bcff014698bfc
Richard Chan <richardchanme@gmail.com>
2017-03-30 00:18:59 -0700

    cleaned up code

--- js/app.js
+++ js/app.js
@@ -7,25 +7,22 @@ function getRandomInt(min, max) {
 
 // Enemies our player must avoid
 var Enemy = function(x,y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+    // Variables applied to each of the game's instances go here,
     this.x = x;
     this.y = y;
     this.speed = getRandomInt(150,500);
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // The image/sprite for our enemies, this uses a helper to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
+    // Need to multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += dt * this.speed;
     this.reset();
-    // this.checkCollisions();
 };
 
 // Draw the enemy on the screen, required method for game
@@ -39,10 +36,6 @@ Enemy.prototype.reset = function() {
         this.x = -100;
     }
 }
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
 // Player class
 var Player = function(x,y) {
@@ -63,7 +56,8 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Make player move
+// Make player move up, down, right, left with arrows.
+// addEventListener at the bottom of file
 Player.prototype.handleInput = function(allowedKeys) {
     
     switch(allowedKeys) {
@@ -85,7 +79,7 @@ Player.prototype.handleInput = function(allowedKeys) {
     }
 }
 
-// Player wins
+// Player wins then creates an alert in browser and resets player to original position
 // @TODO: add timer of 1 second before player goes back to original position
 Player.prototype.playerWin = function() {
     if (this.y < -45) {
@@ -95,13 +89,13 @@ Player.prototype.playerWin = function() {
     }
 }
 
-// Player wins and reset
+// Player wins and reset - this is invoked in checkCollisions()
 Player.prototype.reset = function() {
     this.x = 300;
     this.y = 500;
 }
 
-// checks for collision between player and enemies
+// Checks for collision between player and enemies - invoked in Player.update
 Player.prototype.checkCollisions = function() {
     var playerSize = {w: 20, h: 30}
     var allEnemiesSize = {w: 40, h: 20}
@@ -122,7 +116,6 @@ Player.prototype.checkCollisions = function() {
 Player.prototype.boundaries = function() {
     if (this.x < 0) {
         this.x = 0;
-        console.log("hitting 707")
     } else if (this.x > 600) {
         this.x = 600;
     } else if (this.y < 0) {
@@ -132,16 +125,14 @@ Player.prototype.boundaries = function() {
     }
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
+// Place all enemy objects in canvas
 var allEnemies = [new Enemy(-150,50), new Enemy(-150,130), new Enemy(-150,220), new Enemy(-150,300),
                 new Enemy(-50,50), new Enemy(0,50), new Enemy(-20,130)];
 
-// Place the player object in a variable called player
+// Place the player object in canvas
 var player = new Player(300,500);
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to Player.handleInput() method
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
